b943fafcf3f291d2dc4b1b04cc29a494d955237c
simraimran <113390014+simraimran@users.noreply.github.com>
2022-11-25 18:33:12 -0500

    Add files via upload

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports=
 {
-    "URI":"mongodb+srv://simra:gxC0Hl7dTOc6dwAp@cluster0.kw8uqzb.mongodb.net/movieStore"
+    "URI":"mongodb+srv://simra:gxC0Hl7dTOc6dwAp@cluster0.kw8uqzb.mongodb.net/?retryWrites=true&w=majority"
 }
 
